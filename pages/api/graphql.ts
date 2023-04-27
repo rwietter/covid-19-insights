9e7da3ffb6c56d7f408169db172b7210bcb8fc56
Maurício Witter <mauriciobw17@gmail.com>
2023-04-27 15:42:30 -0300

    feat(api): create GraphQL web server

---
+++ pages/api/graphql.ts
@@ -0,0 +1,19 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export default startServerAndCreateNextHandler(server);
