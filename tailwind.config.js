04eeaec29ae3386dd70cfc770e72864c8b95ea96
Maurício Witter <mauriciobw17@gmail.com>
2023-05-31 00:34:49 -0300

    feat(header): create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        background: '#171821',
+        foreground: '#F5F5F5',
+        primary: '#171821',
+        secondary: '#21222D'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
       }
     }
   },
