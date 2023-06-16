2d7882ce02f8790bf028e6d997289b93d35f5845
Maurício Witter <mauriciobw17@gmail.com>
2023-06-16 10:35:22 -0300

    fix(header): fixed header

--- domains/dashboard/features/Header/Header.tsx
+++ domains/dashboard/features/Header/Header.tsx
@@ -5,8 +5,8 @@ import { useSelectDate } from '@/domains/dashboard/hooks/useSelectDate';
 const Header = () => {
   const { onChangeDate } = useSelectDate();
   return (
-    <header className='bg-secondary text-foreground w-full h-12 flex items-center justify-between px-5 md:px-8 py-1'>
-      <section className='flex items-center gap-3'>
+    <header className='bg-secondary bg-opacity-80 fixed border-b border-slate-300 backdrop-blur-sm text-foreground z-50 w-full h-12 flex items-center justify-center sm:justify-between px-5 md:px-8 py-1'>
+      <section className='items-center gap-3 hidden sm:flex'>
         <Image src="/virus.svg" alt="Logo" width={30} height={30} />
         <h1 className='text-slate-800 font-sans font-semibold'>Covid Insights</h1>
       </section>
