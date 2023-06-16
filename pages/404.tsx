8444ba088e44029adb4d84ddbfc8068e2fd02be6
Maurício Witter <mauriciobw17@gmail.com>
2023-06-16 15:11:13 -0300

    feat(getstaticprops): add error 404 error handling to the getStaticProps

---
+++ pages/404.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Page404 = () => {
+  return (
+    <div className='relative w-full h-screen overflow-hidden'>
+      <div className="flex flex-col justify-center absolute z-10 w-full h-full items-start p-10 lg:p-72 bg-white">
+        <h1 className="text-9xl font-bold text-slate-600">
+          40
+          <span className="rotate-3 text-9xl">4</span>
+        </h1>
+        <h2 className="text-2xl text-slate-700 pt-4 font-semibold">OOPS... PÁGINA NÃO ENCONTRADA :(</h2>
+        <p className="text-lg pt-2 max-w-xl text-slate-700">Nós sugerimos voltar para a página inicial enquanto tentamos resolver o problema.</p>
+        <Link
+          href="/"
+          className="block cursor-pointer bg-purple-500 mt-10 hover:bg-purple-600 transition-colors text-white font-bold py-3 px-4 text-center rounded"
+          role='button'
+        >
+          Voltar para a página inicial
+        </Link>
+      </div>
+      <div className='w-full h-full flex justify-between p-96 relative'>
+        <Image
+          src="/use292.svg"
+          alt="404"
+          className='absolute z-30 left-0 top-0'
+          width={400}
+          height={400}
+        />
+        <Image
+          src="/use308.svg"
+          alt="404"
+          className='absolute z-30 right-0 bottom-0'
+          width={400}
+          height={400}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,26 +11,34 @@ export default function Page(props: PageProps) {
 }
 
 export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<QueryProps>> => {
-  const { data, error } = await client.query({
-    query: clinicalDataQuery,
-    variables: {
-      startDate: '2022-10-03T10:15:30Z',
-      endDate: '2022-12-03T10:15:30Z'
-    }
-  });
-
-  if (error != null) {
-    return {
-      notFound: true
-    };
-  }
-
-  return {
-    props: {
-      countPatientsSymptoms: data.countPatientsSymptoms,
-      countPatients: data.countPatients,
-      countPatientsByAge: data.countPatientsByAge
+  const notFound = {
+    redirect: {
+      destination: '/404',
+      permanent: false
     },
-    revalidate: 3600 // 1 hour
+    revalidate: 400 // 6 minutes
   };
+
+  try {
+    const { data, error } = await client.query({
+      query: clinicalDataQuery,
+      variables: {
+        startDate: '2022-01-01T10:00:00Z',
+        endDate: '2022-12-31T10:00:00Z'
+      }
+    });
+
+    if (error != null) return { ...notFound };
+
+    return {
+      props: {
+        countPatientsSymptoms: data.countPatientsSymptoms,
+        countPatients: data.countPatients,
+        countPatientsByAge: data.countPatientsByAge
+      },
+      revalidate: 3600 // 1 hour
+    };
+  } catch (error) {
+    return { ...notFound };
+  }
 };
